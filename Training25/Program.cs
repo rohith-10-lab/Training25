9c5b5c998ad27cebec27fae9b1edf49e7812c428
rohith-10-lab <Rohithkumar.Kumaresan@trumpf.com>
2025-09-23 11:07:08 +0530

    First commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,10 +5,14 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training25;
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
-   }
+int n1;
+while (true) {
+   Console.Write ("Enter a number: ");
+   string input = Console.ReadLine ();
+   if (int.TryParse (input, out n1)) break;
+   else Console.WriteLine ("Please enter a valid integer");
 }
+for (int i = 1; i <= 10; i++) {
+   Console.WriteLine ($"{n1} * {i,2} = {n1 * i}"); //{i,2} right aligns with width 2
+}
+Console.ReadKey ();
