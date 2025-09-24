7ce898ad01283408c4d85ce8ebb0c3d33cd29135
rohith-10-lab <Rohithkumar.Kumaresan@trumpf.com>
2025-09-24 10:27:33 +0530

    reviewed fix1

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -6,14 +6,18 @@
 // Program on T05 - Multiplication tables. 
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
+
 internal class Program {
    static void Main (string[] args) => Multiply ();
 
-   /// <summary> prints tables from 1 to 10</summary>
+   /// <summary>Prints multiplication tables from 1 to 10</summary>
    static void Multiply () {
       for (int i = 1; i <= 10; i++) {
-         for (int j = 1; j <= 10; j++) Console.WriteLine ($"{i} * {j,2} = {i * j}"); //{j,2} right aligns with width 2
+         for (int j = 1; j <= 10; j++)
+            Console.WriteLine ($"{i} * {j,2} = {i * j}"); //{j,2} right aligns with width 2
          Console.WriteLine ();
       }
    }
 }
+
+
