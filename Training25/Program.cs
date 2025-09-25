63b86774b90507117920a53ca95e84772c76641a
rohith-10-lab <Rohithkumar.Kumaresan@trumpf.com>
2025-09-25 09:26:09 +0530

    Optimised GetInput

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -8,37 +8,23 @@
 namespace Training25;
 
 internal class Program {
-   static void Main (string[] args) {
-      string input = GetInput ();
-      ArmStrong (input);
-   }
-   /// <summary> Gets only postive integer from the user </summary>
-   static string GetInput () {
-      int n = 0;
+   static void Main () => ArmStrong (GetInput ());
+
+   /// <summary>Gets only postive integer from the user</summary>
+   static int GetInput () {
       while (true) {
          Console.Write ("Enter a positive integer: ");
-         string? input = Console.ReadLine (); //Can accept null value
-         if (string.IsNullOrWhiteSpace (input)) {
-            Console.WriteLine ("Input cannot be empty");
-            continue; //Asks user again for input
-         }
-         bool isValid = int.TryParse (input, out n); //Checks whether the input is int
-         if (!isValid || n < 0) {
-            Console.WriteLine ("Enter a valid integer");
-            continue;
-         }
-         break;
+         if (int.TryParse (Console.ReadLine (), out int n) && n >= 0) return n;
+            Console.WriteLine ("Enter a valid input");
       }
-      string tempString = n.ToString ();
-      return tempString;
    }
-   /// <summary> Checks whether a number is armstrong number or not </summary>
+
+   /// <summary>Checks whether a number is armstrong number or not</summary>
    static void ArmStrong (string tempString) {
       double totalsum = 0;
       for (int i = 0; i < tempString.Length; i++) {
-         int digit = int.Parse (tempString[i].ToString ()); //Store each index of the string as int
-         double sum = Math.Pow (digit, tempString.Length);
-         totalsum += sum;
+         int digit = int.Parse (tempString[i].ToString ()); // Store each index of string as int
+         totalsum += Math.Pow (digit, tempString.Length); ;
       }
       if (totalsum == double.Parse (tempString)) Console.WriteLine ("It is an armstrong number");
       else Console.WriteLine ("It is not an armstrong number");
