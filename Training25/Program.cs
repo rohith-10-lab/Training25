5cbc9a0d8a3540796fb7dce1f62423e0777fe7b3
rohith-10-lab <Rohithkumar.Kumaresan@trumpf.com>
2025-09-25 09:36:38 +0530

    Review Fix6

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -10,12 +10,12 @@ namespace Training25;
 internal class Program {
    static void Main () => Multiply ();
 
-   ///<summary>Prints multiplication tables from 1 to 10</summary>
+   /// <summary>Prints multiplication tables from 1 to 10</summary>
    static void Multiply () {
-      int k = 1;
-      for (int i = k; i <= 10; i++) {
-         for (int j = k; j <= 10; j++)
-            Console.WriteLine ($"{i} * {j,2} = {i * j}"); //{j,2} right aligns with width 2
+      int start = 1, end = 10;
+      for (int i = start; i <= end; i++) {
+         for (int j = start; j <= end; j++)
+            Console.WriteLine ($"{i} * {j,2} = {i * j}"); // {j,2} right aligns with width 2
          Console.WriteLine ();
       }
    }
