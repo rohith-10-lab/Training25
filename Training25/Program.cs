30d936b90b799e8ebc9992fd97e5dd7cb9993188
rohith-10-lab <Rohithkumar.Kumaresan@trumpf.com>
2025-09-29 10:57:46 +0530

    Review fix5

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -10,16 +10,11 @@ namespace Training25;
 
 internal class Program {
    static void Main () {
-      int input = GetInput ();
-      // Digital root : compresses any number to its single digit sum
-      WriteLine ($"The digital root of {input} is {(input == 0 ? 0 : 1 + ((input - 1) % 9))}");
-   }
-
-   // Gets only positive integer from the user
-   static int GetInput () {
+      int input;
+      // Gets only positive number from the user
       while (true) {
          Write ("Enter a positive integer: ");
-         if (int.TryParse (ReadLine (), out int number) && number >= 0) return number;
+         if (int.TryParse (Console.ReadLine (), out input) && input >= 0) break;
          ForegroundColor = ConsoleColor.Yellow;
          WriteLine ("Enter a valid input");
          ResetColor ();
@@ -27,5 +22,7 @@ internal class Program {
          ReadKey ();
          Clear ();
       }
+      // Digital root : compresses any number to its single digit sum
+      WriteLine ($"The digital root of {input} is {(input == 0 ? 0 : 1 + ((input - 1) % 9))}");
    }
 }
