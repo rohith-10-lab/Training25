13737411a94540802d734e06a1270104cb0d95e0
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-10-09 08:59:21 +0530

    Review fix2

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -7,6 +7,7 @@
 // ------------------------------------------------------------------------------------------------
 using System.Text;
 using static System.Console;
+using static System.ConsoleColor;
 
 namespace Training25;
 
@@ -15,22 +16,17 @@ internal class Program {
       while (true) {
          Write ("Enter a password: ");
          string? pwd = ReadLine ();
-         if (string.IsNullOrEmpty (pwd)) {
-            Msg ("Enter a valid input\n");
-            continue;
-         }
-         if (TryParsePwd (pwd, out string errors)) {
-            Msg ("Password is strong", ConsoleColor.Green);
-            break;
-         } else {
-            Msg ("Password is weak", ConsoleColor.Red);
-            Msg (errors);
-         }
+         if (string.IsNullOrEmpty (pwd)) { Msg ("Enter a valid input\n"); continue; }
+         bool isPwdStrong = TryParsePwd (pwd, out string error);
+         Msg (isPwdStrong ? "Password is strong" : "Password is weak",
+            isPwdStrong ? Green : Red);
+         if (isPwdStrong) break;
+         Msg (error);
       }
    }
 
    // Checks password rules and returns error messages for violations
-   static bool TryParsePwd (string pwd, out string errors) {
+   static bool TryParsePwd (string pwd, out string error) {
       var sb = new StringBuilder ();
       if (pwd.Length < 6) sb.AppendLine ("Password should have length of at least 6");
       if (pwd.Any (char.IsWhiteSpace)) sb.AppendLine ("Password shouldn't have space");
@@ -41,12 +37,12 @@ internal class Program {
          sb.AppendLine ("Password should have at least one Uppercase letter");
       if (!pwd.Any (c => !char.IsLetterOrDigit (c)))
          sb.AppendLine ("Password should have at least one special character");
-      errors = sb.ToString ();
-      return string.IsNullOrEmpty (errors);
+      error = sb.ToString ();
+      return string.IsNullOrEmpty (error);
    }
 
    // Prints the message in the given colour
-   static void Msg (string msg, ConsoleColor color = ConsoleColor.Yellow) {
+   static void Msg (string msg, ConsoleColor color = Yellow) {
       ForegroundColor = color;
       WriteLine (msg);
       ResetColor ();
