ca9f2a71be67f72ecb7d0999adcfa647b1dd94fe
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-10-13 17:18:44 +0530

    Review fix2

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -12,11 +12,11 @@ namespace Training25;
 internal class Program {
    static void Main (string[] args) {
       if (args.Length == 0) {
-         WriteLine ("Usage: Training25.exe <number>");
+         WriteLine ("Usage: Training25.exe <numbers from 1 to 25>");
          return;
       }
       if (!int.TryParse (args[0], out int inp) || inp <= 0 || inp > 25) {
-         WriteLine ("Enter a valid integer between 1 and 25.");
+         WriteLine ("Enter a valid integer from 1 and 25.");
          return;
       }
       WriteLine (NthArmstrong (inp));
@@ -25,25 +25,19 @@ internal class Program {
    // Returns the Nth armstrong number
    static int NthArmstrong (int inp) {
       int cnt = 0, num = 0;
-      while (true) {
-         if (IsArmstrong (num)) {
-            cnt++;
-            if (cnt == inp) return num;
-         }
-         num++;
-      }
+      for (; ; num++) if (IsArmstrong (num) && ++cnt == inp) return num;
    }
 
    // Checks whether a given number is armstrong or not
    static bool IsArmstrong (int num) {
       string numStr = num.ToString ();
       int sum = 0;
-      foreach (char index in numStr) sum += IntPow (index - '0', numStr.Length);
+      foreach (char index in numStr) sum += PowerOf (index - '0', numStr.Length);
       return sum == num;
    }
 
    // Computes the power of a number in int
-   static int IntPow (int num, int exp) {
+   static int PowerOf (int num, int exp) {
       int res = 1;
       for (int i = 0; i < exp; i++) res *= num;
       return res;
