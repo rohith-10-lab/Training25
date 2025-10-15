b790c7b035b77ffd6c07673543832686281a3eb8
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-10-15 15:42:20 +0530

    T10: Reverse the string

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,43 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to reverse a string while maintaining spaces and casing
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+using System.Text;
+
 namespace Training25;
+
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () {
+      WriteLine ($"The reversed string is: {ReverseStr (GetInput ())}");
+   }
+
+   // Gets only non-empty string from the user
+   static string GetInput () {
+      while (true) {
+         Write ("Enter the string to be reversed: ");
+         string? inp = ReadLine ();
+         if (string.IsNullOrWhiteSpace (inp)) WriteLine ("Enter a valid input\n");
+         else return inp;
+      }
+   }
+
+   // Reverses a string, maintaining spaces and casing
+   static string ReverseStr (string inp) {
+      char[] chars = inp.Where (c => !char.IsWhiteSpace (c)).ToArray ();
+      Array.Reverse (chars);
+      var res = new StringBuilder ();
+      int idx = 0;
+      foreach (char c in inp) {
+         if (c == ' ') res.Append (' ');
+         else {
+            char newChar = chars[idx];
+            if (char.IsUpper (c)) res.Append (char.ToUpper (newChar));
+            else res.Append (char.ToLower (newChar));
+            idx++;
+         }
+      }
+      return res.ToString ();
    }
 }
