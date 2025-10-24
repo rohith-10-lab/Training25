bbca1d34475197ff243a71097a20c02dd0a03ad0
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-10-24 15:47:20 +0530

    T12: Voting contest

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,30 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to return the most frequent character in a string and its occurence.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
+
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () => WriteLine ($"{MostFrequentChar (GetInput (), out int cnt)},{cnt}");
+
+   // Gets only string of letters from user as input
+   static string GetInput () {
+      while (true) {
+         Write ("Enter a string of alphabets: ");
+         string? inp = ReadLine ();
+         if (string.IsNullOrEmpty (inp) || inp.Any (c => !char.IsLetter (c)))
+            WriteLine ("Enter a valid input\n");
+         else return inp;
+      }
+   }
+
+   // Returns the most frequent character in the string and its count through out parameter
+   static char MostFrequentChar (string inp, out int cnt) {
+      var firstMax = inp.ToLower ().GroupBy (c => c).OrderByDescending (g => g.Count ()).First ();
+      cnt = firstMax.Count ();
+      return firstMax.Key;
    }
 }
