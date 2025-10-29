47af3d46ce9619542e1995698704676fe1a35175
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-10-29 12:32:09 +0530

    Test03: Magic square

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,27 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to check if it is a magic square.
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   // To store 9 values in a array
+   static void Main () {
+      int[] arr = new int[9];
+      for (int i = 1; i <= 9; i++) {
+         Console.Write ($"Number {i}: ");
+         if (int.TryParse (Console.ReadLine (), out int cnt))
+         arr[i - 1] = cnt;
+      }
+      Console.WriteLine (arr);
+   }
+
+   // To check the conditions and return true or false
+   static bool Checktrue (int[] b) {
+      if (b[0] + b[1] + b[2] == b[3] + b[4] + b[5] == b[6] + b[7] + b[8] // rows
+         == b[0] + b[3] + b[6] == b[1] + b[4] + b[7] == b[2] + b[5] + b[8] // columns
+         == b[0] + b[4] + b[8] == b[2] + b[4] + b[6]) // diagonals
+         return true;
+      else return false;
    }
 }
