786166d65b9dfe85e47932c6a156540bda92182a
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-10-29 11:51:12 +0530

    draft commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,9 +5,26 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
+using System.Text;
+
 namespace Training25;
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () {
+      int inp = 52;
+      char[] chars = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S',
+      'T','U','V','W', 'X','Y','Z'};
+      int len = chars.Length;
+      var res = new StringBuilder ();
+      if (inp > len) {
+         res.Append (chars [0]);
+         int rem = inp / len;
+         while (rem > len) {
+            res.Append ((chars[0]));
+            rem = rem / len;
+         }
+         res.Append (chars[rem - 1]);
+         Console.WriteLine (res.ToString ());
+      }
+      else Console.WriteLine (chars[inp-1]);
    }
 }
