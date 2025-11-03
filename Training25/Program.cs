305f7a44c2ce254f04e34e01eca0b6f0c87090a6
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-11-03 17:07:13 +0530

    T14: Smallest Transform

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,30 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to return the minimum steps to make all the digits of a number identical.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
+
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () => WriteLine ($"{SmallestTransform (Getinput ())} steps");
+
+   // Gets only integer as input from the user
+   static int Getinput () {
+      while (true) {
+         Write ("Enter the number to transform: ");
+         if (int.TryParse (ReadLine (), out int inp) && inp > 0) return inp;
+         else WriteLine ("Enter a valid input.\n");
+      }
+   }
+
+   // Returns the minimum steps to make all the digits of a number identical
+   static int SmallestTransform (int inp) {
+      string inpStr = inp.ToString ();
+      string sort = string.Concat (inpStr.OrderBy (c => c));
+      int median = sort[inpStr.Length / 2] - '0', sum = 0;
+      foreach (char c in inpStr) sum += Math.Abs (c - '0' - median);
+      return sum;
    }
 }
