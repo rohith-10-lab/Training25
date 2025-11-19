6d46a4c3e05456e61de701294d665b037e025fe8
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-11-19 14:56:06 +0530

    Review fix 2

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -15,24 +15,19 @@ internal class Program {
       OutputEncoding = new UnicodeEncoding ();
       WriteLine (PrintBoard (Pieces ()));
    }
-   const int SIZE = 8;
-   static readonly string[] sBlackSpecial = ["\u265C", "\u265E", "\u265D", "\u265B",
-                                            "\u265A", "\u265D", "\u265E", "\u265C"];
-   static readonly string[] sWhiteSpecial = ["\u2656", "\u2658", "\u2657", "\u2655",
-                                            "\u2654", "\u2657", "\u2658", "\u2656"];
-   static readonly string sBlackPawn = "\u265F";
-   static readonly string sWhitePawn = "\u2659";
 
    // Returns 8x8 array with chess piece positions
    static string[,] Pieces () {
       string[,] board = new string[SIZE, SIZE];
       for (int row = 0; row < SIZE; row++) {
          for (int col = 0; col < SIZE; col++) {
-            if (row == 0) board[row, col] = sBlackSpecial[col];
-            else if (row == 1) board[row, col] = sBlackPawn;
-            else if (row >= 2 && row <= 5) board[row, col] = " ";
-            else if (row == 6) board[row, col] = sWhitePawn;
-            else board[row, col] = sWhiteSpecial[col];
+            board[row, col] = row switch {
+               0 => sBlackSpecial[col],
+               1 => BLACKPAWN,
+               >= 2 and <= 5 => " ",
+               6 => WHITEPAWN,
+               _ => sWhiteSpecial[col]
+            };
          }
       }
       return board;
@@ -41,18 +36,26 @@ internal class Program {
    // Returns the chessboard with pieces
    static string PrintBoard (string[,] board) {
       var sb = new StringBuilder ();
-      var segments = Enumerable.Repeat ("\u2500\u2500\u2500\u2500", SIZE);
-      string top = "\u250C" + string.Join ("\u252C", segments) + "\u2510";
+      var segments = Enumerable.Repeat ("\u2500\u2500\u2500", SIZE);
       string mid = "\u251C" + string.Join ("\u253C", segments) + "\u2524";
-      string bottom = "\u2514" + string.Join ("\u2534", segments) + "\u2518";
-      sb.AppendLine (top);
+      // Top part of board
+      sb.AppendLine ("\u250C" + string.Join ("\u252C", segments) + "\u2510");
       for (int row = 0; row < SIZE; row++) {
-         sb.Append ('\u2502');
-         for (int col = 0; col < SIZE; col++) sb.Append ($" {board[row, col]}  \u2502");
+         sb.Append (BORDER);
+         for (int col = 0; col < SIZE; col++) sb.Append ($" {board[row, col]} {BORDER}");
          sb.AppendLine ();
+         // Middle part of board
          if (row < SIZE - 1) sb.AppendLine (mid);
       }
-      sb.AppendLine (bottom);
+      // Bottom part of board
+      sb.AppendLine ("\u2514" + string.Join ("\u2534", segments) + "\u2518");
       return sb.ToString ();
    }
+   const int SIZE = 8;
+   static readonly string[] sBlackSpecial = ["\u265C", "\u265E", "\u265D", "\u265B",
+                                            "\u265A", "\u265D", "\u265E", "\u265C"];
+   static readonly string[] sWhiteSpecial = ["\u2656", "\u2658", "\u2657", "\u2655",
+                                            "\u2654", "\u2657", "\u2658", "\u2656"];
+   const string BLACKPAWN = "\u265F", WHITEPAWN = "\u2659";
+   const char BORDER = '\u2502';
 }
